5ffa7a030708ba406735550b00aa099f727d08fb
MNIO1900 <mnio.mynameisoleg@gmail.com>
2026-09-03 02:19:39 +0300

    Update ResourceKeysDictionary.cs

--- src/Gml.Launcher/Assets/Resources/ResourceKeysDictionary.cs
+++ src/Gml.Launcher/Assets/Resources/ResourceKeysDictionary.cs
@@ -3,6 +3,6 @@
 public static class ResourceKeysDictionary
 {
     public const string Host = "https://gmlf.recloud.tech";
-    public const string SecondaryHost = "https://youtu.be/FQZGzEOBd_w";
+    public const string SecondaryHost = "http://135.106.198.136:5003";
     public const string FolderName = "FallOrder";
 }
